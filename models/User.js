0873635485ad0259c2f498e0cccd7bca2508e044
Vaibhav Bura <vaibhavbura@gmail.com>
2025-10-07 00:55:50 +0530

    fix: define userSchema properly for mongoose

--- models/User.js
+++ models/User.js
@@ -1,13 +1,16 @@
 import mongoose from "mongoose";
 
-userSchema = new mongoose.Schema({
-    _id:{type:String,required:true},
-    name:{type:String,required:true},
-    email:{type:String,required:true,unique:true},
-    imageUrl : {type:String,required:true},
-    cardItems:{type:Object,default:{}}
-},{minimize:false})
+const userSchema = new mongoose.Schema(
+  {
+    _id: { type: String, required: true },
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    imageUrl: { type: String, required: true },
+    cartItems: { type: Object, default: {} },
+  },
+  { minimize: false }
+);
 
-const User = mongoose.model.user || mongoose.model('user',userSchema)
+const User = mongoose.models.user || mongoose.model("user", userSchema);
 
-export default User
+export default User;
